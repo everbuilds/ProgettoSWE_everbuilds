c4c080a4696653c43778bc4fc2a4de309e0cea45
marcotesser <marco.tesser.1@studenti.unipd.it>
2021-02-22 13:46:13 +0100

    modifica UI

--- app/src/main/java/com/example/rgp_project/Game.kt
+++ app/src/main/java/com/example/rgp_project/Game.kt
@@ -10,10 +10,21 @@ import android.graphics.drawable.shapes.RoundRectShape
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.util.Log
+import android.view.MotionEvent
+import android.view.VelocityTracker
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 
 class Game : AppCompatActivity() {
+
+    private var mVelocityTracker: VelocityTracker? = null
+    var Xposition = 25
+    var Yposition = 600
+    //var right = 50
+    //var bottom = 50
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
@@ -27,24 +38,33 @@ class Game : AppCompatActivity() {
         val bitmap: Bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         val canvas: Canvas = Canvas(bitmap)
 
-        var left = 100
-        var top = 100
-        var right = 600
-        var bottom = 400
-
         var shapeDrawable: ShapeDrawable
         shapeDrawable = ShapeDrawable(RectShape())
-        shapeDrawable.setBounds( left, top, right, bottom)
+        shapeDrawable.setBounds( Xposition-25, Yposition-25 ,Xposition+25,Yposition+25)
         shapeDrawable.getPaint().setColor(Color.parseColor("#009944"))
         shapeDrawable.draw(canvas)
 
 
         val imageV = findViewById<ImageView>(R.id.imageV)
         imageV.background = BitmapDrawable(getResources(), bitmap)
-
-
-
     }
+    override fun onTouchEvent(event: MotionEvent): Boolean {
 
-
+        val x : Float = event.x
+        val y : Float = event.y
+        when (event.actionMasked) {
+            MotionEvent.ACTION_MOVE -> {
+                /*var dx = x-Xposition
+                var dy = y-Yposition*/
+                Xposition = x.toInt()
+                Yposition = y.toInt()
+                //println(Xposition)
+                //println(Yposition)
+            }
+            MotionEvent.ACTION_UP -> {
+                var view : View =  
+            }
+        }
+        return true
+    }
 }
